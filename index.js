310534ad849b95283f9a996d7efc486982dbe414
Yomaira Escano <yjescano@gmail.com>
2018-08-20 04:44:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ BoardMember.prototype.releasePressStatement = function() {
   return 'You will see great things from Scuber.'
 }
 
+BoardMember.prototype.sayHi = function() {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
+
 // function BoardMember(name, homeState, training){
 //   this.name = name
 //   this.homeState = homeState
